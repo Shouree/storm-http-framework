46c66feb99d09837292b016f21250b531f223a97
Filip Strömbäck <fstromback@gmail.com>
2022-05-05 19:55:42 +0200

    Added automated tests for compiling C/C++ code with Progvis.

---
+++ root/progvis_demo/test/compact.c
@@ -0,0 +1,15 @@
+int main(void) {
+	if(true){
+		printf("A");
+	}else{
+		printf("B");
+	}
+
+	if(false)printf("A");
+	else printf("B");
+
+	int elsefoo;
+	if(false)printf("C");
+	elsefoo;
+	return 0;
+}
