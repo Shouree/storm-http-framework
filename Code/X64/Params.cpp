2b546a93c5baf024f16d1b95bf03f58cde196d68
Filip Strömbäck <fstromback@gmail.com>
2022-12-26 11:31:06 +0100

    WIP: Started porting the X64 backend to the generic Params interface.

--- Code/X64/Params.cpp
+++ Code/X64/Params.cpp
@@ -5,101 +5,13 @@
 namespace code {
 	namespace x64 {
 
-		Param::Param() {
-			clear();
-		}
-
-		Param::Param(Nat id, Primitive p) {
-			set(id, p.size().size64(), p.offset().v64());
-		}
-
-		Param::Param(Nat id, Nat size, Nat offset) {
-			set(id, size, offset);
-		}
-
-		void Param::clear() {
-			set(0xFF, 0, 0xFFFFFFFF);
-		}
-
-		void Param::set(Nat id, Nat size, Nat offset) {
-			data = size & 0xF;
-			data |= (id & 0xFF) << 4;
-			data |= (offset & 0xFFFFFF) << 12;
-		}
-
-		wostream &operator <<(wostream &to, Param p) {
-			if (p.id() == 0xFF) {
-				to << L"empty";
-			} else {
-				to << L"#" << p.id() << L"+" << p.offset() << L"," << p.size();
-			}
-			return to;
-		}
-
-		StrBuf &operator <<(StrBuf &to, Param p) {
-			if (p.id() == 0xFF) {
-				to << S("empty");
-			} else {
-				to << S("#") << p.id() << S("+") << p.offset() << S(",") << p.size();
-			}
-			return to;
-		}
-
-		static void clear(GcArray<Param> *array) {
-			for (Nat i = array->filled; i < array->count; i++)
-				array->v[i].clear();
-		}
-
-		Params::Params() {
-			integer = runtime::allocArray<Param>(engine(), &natArrayType, 6);
-			real = runtime::allocArray<Param>(engine(), &natArrayType, 8);
-			integer->filled = 0;
-			real->filled = 0;
-			clear(integer);
-			clear(real);
-			stack = new (this) Array<Nat>();
-		}
-
-		void Params::add(Nat id, TypeDesc *type) {
-			if (PrimitiveDesc *p = as<PrimitiveDesc>(type)) {
-				add(id, p->v);
-			} else if (ComplexDesc *c = as<ComplexDesc>(type)) {
-				addDesc(id, c);
-			} else if (SimpleDesc *s = as<SimpleDesc>(type)) {
-				addDesc(id, s);
-			} else {
-				assert(false, L"Unknown type description found.");
-			}
-		}
-
-		void Params::add(Nat id, Primitive p) {
-			switch (p.kind()) {
-			case primitive::none:
-				// Nothing to do!
-				break;
-			case primitive::pointer:
-			case primitive::integer:
-				tryAdd(integer, Param(id, p));
-				break;
-			case primitive::real:
-				tryAdd(real, Param(id, p));
-				break;
-			default:
-				dbg_assert(false, L"Unknown primitive type!");
-			}
-		}
-
-		void Params::addDesc(Nat id, ComplexDesc *type) {
-			// The complex types are actually simple. We just add a pointer to our parameter list!
-			tryAdd(integer, Param(id, 8, 0));
-		}
-
-		void Params::tryAdd(GcArray<Param> *to, Param p) {
-			if (to->filled < to->count) {
-				to->v[to->filled++] = p;
-			} else {
-				stack->push(p.id());
-			}
+		static Size roundSize(Size sz) {
+			if (sz.size64() == 1)
+				return sz;
+			else if (sz.size64() <= 4)
+				return Size::sInt;
+			else
+				return sz.alignedAs(Size::sWord);
 		}
 
 		static primitive::Kind merge(primitive::Kind a, primitive::Kind b) {
@@ -148,7 +60,94 @@ namespace code {
 			return result;
 		}
 
-		void Params::addDesc(Nat id, SimpleDesc *type) {
+
+
+		Params::Params() : code::Params(6, 8, 8, 16) {}
+
+		Reg Params::registerSrc(Nat id) {
+			static Reg v[] = {
+				ptrDi, ptrSi, ptrD, ptrC, ptr8, ptr9,
+				xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7
+			};
+			return v[n];
+		}
+
+
+		void Params::resultPrimitive(Primitive p) {
+			if (p.kind() == primitive::real)
+				resultData = Result::inRegister(engine(), asSize(xmm0, p.size()));
+			else
+				resultData = Result::inRegister(engine(), asSize(ptrA, p.size()));
+		}
+
+		void Params::resultComplex(ComplexDesc *desc) {
+			resultData = Result::inMemory(ptrDi);
+			// Add "dummy" parameter:
+			addInt(Param(Param::returnId(), desc->size(), true, 0, true));
+		}
+
+		void Params::resultSimple(SimpleDesc *desc) {
+			Nat size = desc->size().size64();
+
+			// Similar logic to adding parameters.
+			if (size > 2*8) {
+				// Pass it on the stack:
+				resultData = Result::inMemory(ptrDi);
+				// Add "dummy" parameter:
+				addInt(Param(Param::returnId(), desc->size(), true, 0, true));
+				return;
+			}
+
+			primitive::Kind part1 = paramKind(desc->v, 0, 8);
+			primitive::Kind part2 = paramKind(desc->v, 8, 16);
+
+			Size firstSize(min(size, Nat(8)));
+			firstSize = roundSize(firstSize);
+			Size secondSize((size > 8) ? (size - 8) : 0);
+			secondSize = roundSize(secondSize);
+
+			if (part2 == primitive::none)
+				resultData = Result::inRegisters(engine(), 1);
+			else
+				resultData = Result::inRegisters(engine(), 2);
+
+			Reg intReg = ptrA;
+			Reg realReg = xmm0;
+
+			if (part1 == primitive::integer) {
+				resultData.putRegister(asSize(intReg, firstSize), 0);
+				intReg = ptrD;
+			} else if (part1 == primitive::real) {
+				resultData.putRegister(asSize(realReg, firstSize), 0);
+			}
+
+			if (part2 == primitive::integer) {
+				resultData.putRegister(asSize(intReg, secondSize), 8);
+			} else if (part2 = primitive::real) {
+				resultData.putRegister(asSize(realReg, secondSize), 8);
+			}
+		}
+
+
+		void Params::addPrimitive(Nat id, Primitive p) {
+			switch (p.kind()) {
+			case primitive::none:
+				break;
+			case primitive::pointer:
+			case primitive::integer:
+				addInt(Params(id, p, true));
+				break;
+			case primitive::real:
+				addReal(Params(id, p, true);
+				break;
+			}
+		}
+
+		void Params::addComplex(Nat id, ComplexDesc *desc) {
+			addInt(Param(id, desc->size(), true, 0, true));
+		}
+
+		void Params::addSimple(Nat id, SimpleDesc *desc) {
 			// Here, we should check 'type' to see if we shall pass parts of it in registers.
 			// It seems the algorithm works roughly as follows (from the offical documentation
 			// and examining the output of GCC from Experiments/call64.cpp):
@@ -158,168 +157,45 @@ namespace code {
 			//   - if the word contains only floating point numbers, pass them into a real register.
 			//   - otherwise, pass the word in an integer register (eg. int + float).
 
-			Nat size = type->size().size64();
+			Nat size = desc->size().size64();
 			if (size > 2*8) {
 				// Too large: pass on the stack!
-				stack->push(id);
+				addStack(Param(id, desc->size(), true, 0, false));
 				return;
 			}
 
-			primitive::Kind first = paramKind(type->v, 0, 8);
-			primitive::Kind second = paramKind(type->v, 8, 16);
+			primitive::Kind first = paramKind(desc->v, 0, 8);
+			primitive::Kind second = paramKind(desc->v, 8, 16);
 
-			Nat firstSize = min(size, Nat(8));
-			Nat secondSize = (size > 8) ? (size - 8) : 0;
+			Size firstSize(min(size, Nat(8)));
+			firstSize = roundSize(firstSize);
+			Size secondSize((size > 8) ? (size - 8) : 0);
+			secondSize = roundSize(secondSize);
 
-			size_t iCount = integer->filled;
-			size_t rCount = real->filled;
+			Nat intCount = (first == primitive::integer) + (second == primitive::integer);
+			Nat realCount = (first == primitive::real) + (second == primitive::real);
 
-			if (tryAdd(first, Param(id, firstSize, 0)) &&
-				tryAdd(second, Param(id, secondSize, 8))) {
-				// It worked!
+			if (hasInt(intCount) && hasReal(realCount)) {
+				// There is room, add parameters!
+				if (first == primitive::integer)
+					addInt(Param(id, Size(firstSize), true, 0, false));
+				else if (first == primitive::real)
+					addReal(Param(id, Size(firstSize), true, 0, false));
+
+				if (second == primitive::integer)
+					addInt(Param(id, Size(secondSize), true, 8, false));
+				else if (second == primitive::real)
+					addReal(Param(id, Size(secondSize), true, 8, false));
 			} else {
-				// Not enough room. Roll back and pass on the stack instead.
-				integer->filled = iCount;
-				clear(integer);
-				real->filled = rCount;
-				clear(real);
-
-				stack->push(id);
+				// Full, push it on the stack.
+				addStack(Param(id, desc->size(), true, 0, false));
 			}
 		}
 
-		bool Params::tryAdd(primitive::Kind kind, Param p) {
-			GcArray<Param> *use = null;
-			switch (kind) {
-			case primitive::none:
-				return true;
-			case primitive::pointer:
-			case primitive::integer:
-				use = integer;
-				break;
-			case primitive::real:
-				use = real;
-				break;
-			}
-
-			if (use->filled < use->count) {
-				use->v[use->filled++] = p;
-				return true;
-			}
-
-			return false;
-		}
-
-		static void put(StrBuf *to, GcArray<Param> *p, const wchar **names) {
-			for (Nat i = 0; i < p->filled; i++) {
-				*to << S("\n") << names[i] << S(":") << p->v[i];
-			}
-		}
-
-		void Params::toS(StrBuf *to) const {
-			*to << S("Parameters:");
-			const wchar *i[] = { S("RDI"), S("RSI"), S("RDX"), S("RCX"), S("R8"), S("R9") };
-			const wchar *e[] = { S("XMM0"), S("XMM1"), S("XMM2"), S("XMM3"), S("XMM4"), S("XMM5"), S("XMM6"), S("XMM7") };
-			put(to, integer, i);
-			put(to, real, e);
-
-			if (stack->count() > 0) {
-				*to << S("\nOn stack: ") << stack->at(0);
-				for (Nat i = 1; i < stack->count(); i++) {
-					*to << S(", ") << stack->at(i);
-				}
-			}
-		}
-
-		Nat Params::registerCount() const {
-			return integer->count + real->count;
-		}
-
-		Param Params::registerAt(Nat n) const {
-			if (n < integer->count)
-				return integer->v[n];
-			if (n - integer->count < real->count)
-				return real->v[n - integer->count];
-			assert(false, L"Out of bounds.");
-			return Param();
-		}
-
-		Reg Params::registerSrc(Nat n) const {
-			static Reg v[] = {
-				ptrDi, ptrSi, ptrD, ptrC, ptr8, ptr9,
-				xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7
-			};
-			return v[n];
-		}
-
-		Params *params(Array<TypeDesc *> *types) {
-			Params *p = new (types) Params();
-			for (Nat i = 0; i < types->count(); i++)
-				p->add(i, types->at(i));
-			return p;
-		}
-
-		/**
-		 * Result.
-		 */
-
-		Result::Result(TypeDesc *result) {
-			part1 = primitive::none;
-			part2 = primitive::none;
-			memory = false;
-
-			if (PrimitiveDesc *p = as<PrimitiveDesc>(result)) {
-				add(p);
-			} else if (ComplexDesc *c = as<ComplexDesc>(result)) {
-				add(c);
-			} else if (SimpleDesc *s = as<SimpleDesc>(result)) {
-				add(s);
-			} else {
-				assert(false, L"Unknown type description found.");
-			}
-
-			if (part1 == primitive::pointer)
-				part1 = primitive::integer;
-			if (part2 == primitive::pointer)
-				part2 = primitive::integer;
-		}
-
-		void Result::toS(StrBuf *to) const {
-			*to << primitive::name(part1) << S(":") << primitive::name(part2);
-		}
-
-		void Result::add(PrimitiveDesc *desc) {
-			// Just add the primitive to the relevant register.
-			// We do not support >64 bit numbers, so this is fine!
-			part1 = desc->v.kind();
-		}
-
-		void Result::add(ComplexDesc *desc) {
-			// Complex types are easy. They are always passed in memory.
-			memory = true;
-		}
-
-		void Result::add(SimpleDesc *desc) {
-			// The logic here is very similar to 'Params::addDesc'.
-			if (desc->size().size64() > 2*8) {
-				// Too large. Pass it on the stack!
-				memory = true;
-				return;
-			}
-
-			part1 = paramKind(desc->v, 0, 8);
-			part2 = paramKind(desc->v, 8, 16);
-		}
-
-		Result *result(TypeDesc *result) {
-			return new (result) Result(result);
-		}
 
 		Params *layoutParams(TypeDesc *result, Array<TypeDesc *> *params) {
-			TODO(L"Use the generic Params interface here as well!");
 			Params *layout = new (params) Params();
-			if (code::x64::result(result)->memory)
-				layout->add(Param::returnId, ptrPrimitive());
+			layout->result(result);
 			for (Nat i = 0; i < params->count(); i++)
 				layout->add(i, params->at(i));
 			return layout;
--- Code/X64/Params.h
+++ Code/X64/Params.h
@@ -1,8 +1,5 @@
 #pragma once
-#include "../TypeDesc.h"
-#include "../Reg.h"
-#include "Core/Object.h"
-#include "Core/Array.h"
+#include "Code/Params.h"
 
 namespace code {
 	namespace x64 {
@@ -24,161 +21,53 @@ namespace code {
 		 *   responsible for copying the parameter and destroying it when the function call is
 		 *   completed.
 		 */
-
-
-		/**
-		 * Describes what a single register is supposed to contain.
-		 */
-		class Param {
-			STORM_VALUE;
-		public:
-			// Create an empty parameter.
-			STORM_CTOR Param();
-			STORM_CTOR Param(Nat id, Primitive p);
-			STORM_CTOR Param(Nat id, Nat size, Nat offset);
-
-			// Reset.
-			void clear();
-
-			// Set to values.
-			void set(Nat id, Nat size, Nat offset);
-
-			// Get the different fields stored in here:
-			inline Nat size() const {
-				return data & 0xF;
-			}
-			inline Nat id() const {
-				return (data & 0xFF0) >> 4;
-			}
-			inline Nat offset() const {
-				return (data & 0xFFFFF000) >> 12;
-			}
-
-			// Compare for equality.
-			inline Bool STORM_FN operator ==(Param o) const {
-				return data == o.data;
-			}
-			inline Bool STORM_FN operator !=(Param o) const {
-				return data != o.data;
-			}
-
-			// ID usable for a hidden return parameter.
-			enum { returnId = 0xFF };
-
-		private:
-			// Stored as follows:
-			// Bits 0-3: Size of the register (in bytes).
-			// Bits 4-11: Parameter number. 0xFF = unused.
-			// Bits 12-31: Offset into parameter (in bytes).
-			Nat data;
-		};
-
-		// Output.
-		wostream &operator <<(wostream &to, Param p);
-		StrBuf &STORM_FN operator <<(StrBuf &to, Param p);
-
-
-		/**
-		 * Describes how parameters are laid out during a function call. This is specific to X86-64,
-		 * so any sizes and offsets stored here are for that specific platform.
-		 */
-		class Params : public storm::Object {
+		class Params : public code::Params {
 			STORM_CLASS;
 		public:
 			// Create an empty parameter layout.
 			STORM_CTOR Params();
 
-			// Add a single parameter to here.
-			void STORM_FN add(Nat id, TypeDesc *type);
-
-			// Add a primitive.
-			void STORM_FN add(Nat id, Primitive p);
-
-			// To string.
-			virtual void STORM_FN toS(StrBuf *to) const;
-
-			/**
-			 * Access to the stack layout.
-			 */
-
-			// Get the total number of elements on the stack.
-			Nat STORM_FN stackCount() const {
-				return stack->count();
-			}
-
-			// Get stack element number 'n'.
-			Nat STORM_FN stackAt(Nat n) const {
-				return stack->at(n);
-			}
-
-			/**
-			 * Access to the registers.
-			 */
-
-			// Total number of registers to examine.
-			Nat STORM_FN registerCount() const;
-
-			// Get a parameter. Might be empty.
-			Param STORM_FN registerAt(Nat n) const;
-
 			// Get the register containing the parameter.
-			Reg STORM_FN registerSrc(Nat n) const;
+			Reg STORM_FN registerSrc(Nat n);
 
-		private:
-			// Available registers:
+		protected:
+			void STORM_FN resultPrimitive(Primitive p);
+			void STORM_FN resultComplex(ComplexDesc *desc);
+			void STORM_FN resultSimple(SimpleDesc *desc);
 
-			// Regular integer registers.
-			GcArray<Param> *integer;
-			// Real registers, for floating point parameters.
-			GcArray<Param> *real;
-			// Stores all parameters that are to be passed on the stack.
-			Array<Nat> *stack;
-
-			// Add complex and simple structs.
-			void addDesc(Nat id, ComplexDesc *type);
-			void addDesc(Nat id, SimpleDesc *type);
-
-			// Try to add a parameter to 'to', otherwise add it to the stack.
-			void tryAdd(GcArray<Param> *to, Param p);
-
-			// Try to add a parameter to a register described by 'kind'. Otherwise, do nothing.
-			bool tryAdd(primitive::Kind kind, Param p);
+			void STORM_FN addPrimitive(Nat id, Primitive p);
+			void STORM_FN addComplex(Nat id, ComplexDesc *desc);
+			void STORM_FN addSimple(Nat id, SimpleDesc *desc);
 		};
-
-		// Create a 'params' object from a list of TypeDesc objects.
-		Params *STORM_FN params(Array<TypeDesc *> *types);
-
-		/**
-		 * Describes how the return value is stored.
-		 */
-		class Result : public storm::Object {
-			STORM_CLASS;
-		public:
-			// Create.
-			STORM_CTOR Result(TypeDesc *type);
-
-			// We're using a maximum of 2 registers. Reg1 is always at offset 0 and reg2 at offset 8.
-			primitive::PrimitiveKind part1;
-			primitive::PrimitiveKind part2;
-
-			// Return in memory?
-			Bool memory;
-
-			// To string.
-			virtual void STORM_FN toS(StrBuf *to) const;
-
-		private:
-			// Helper functions.
-			void add(PrimitiveDesc *desc);
-			void add(ComplexDesc *desc);
-			void add(SimpleDesc *desc);
-		};
-
-		// Create a 'result' object describing how the return value shall be represented.
-		Result *STORM_FN result(TypeDesc *type);
 
 		// Perform a full layout of parameters.
 		Params *STORM_FN layoutParams(TypeDesc *result, Array<TypeDesc *> *params);
+
+		// /**
+		//  * Describes how the return value is stored.
+		//  */
+		// class Result : public storm::Object {
+		// 	STORM_CLASS;
+		// public:
+		// 	// Create.
+		// 	STORM_CTOR Result(TypeDesc *type);
+
+		// 	// We're using a maximum of 2 registers. Reg1 is always at offset 0 and reg2 at offset 8.
+		// 	primitive::PrimitiveKind part1;
+		// 	primitive::PrimitiveKind part2;
+
+		// 	// Return in memory?
+		// 	Bool memory;
+
+		// 	// To string.
+		// 	virtual void STORM_FN toS(StrBuf *to) const;
+
+		// private:
+		// 	// Helper functions.
+		// 	void add(PrimitiveDesc *desc);
+		// 	void add(ComplexDesc *desc);
+		// 	void add(SimpleDesc *desc);
+		// };
 
 	}
 }
