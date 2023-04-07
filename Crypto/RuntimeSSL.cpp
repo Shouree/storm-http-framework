e0b120fcb5cabfcc70ecd8077bc80c67e12e4a9e
Filip Strömbäck <fstromback@gmail.com>
2023-04-07 16:51:33 +0200

    Fixed accidental version dependency.

--- Crypto/RuntimeSSL.cpp
+++ Crypto/RuntimeSSL.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "RuntimeSSL.h"
 #include "Exception.h"
+#include "Core/Convert.h"
 
 #ifdef POSIX
 
@@ -20,20 +21,30 @@
 	extern "C" ret name params {				\
 		return (*ptr_ ## name)names;			\
 	}
+#define SSL_FN_ALT(lib, ret, name, params, names, alt)	\
+	static ret (*ptr_ ## name) params;					\
+	extern "C" ret name params {						\
+		return (*ptr_ ## name)names;					\
+	}
 #include "OpenSSLFunctions.inc"
 #undef SSL_FN
+#undef SSL_FN_ALT
 
 namespace ssl {
 
 	static util::Lock initLock;
 	static bool initialized = false;
 
-	void *loadFn(void *from, const char *name) {
+	void *loadFn(void *from, const char *name, const char *altName = null) {
 		void *r = dlsym(from, name);
-		std::cout << name << ": " << r << std::endl;
+		if (altName && !r)
+			r = dlsym(from, altName);
+
 		if (!r) {
 			StrBuf *msg = new (runtime::someEngine()) StrBuf();
-			*msg << S("Can not find the function ") << name << S(" in the SSL or crypto library.");
+			*msg << S("Can not find the function ")
+				 << new (msg) Str(toWChar(msg->engine(), name))
+				 << S(" in the SSL or crypto library.");
 			throw new (msg) SSLError(msg->toS());
 		}
 		return r;
@@ -53,11 +64,13 @@ namespace ssl {
 		if (!libSSL || !libCrypto)
 			throw new (runtime::someEngine()) SSLError(S("Unable to load libssl.so and/or libcrypto.so. Make sure they are installed!"));
 
-		// ptr_SSL_CTX_free = (void (*)(SSL_CTX *ctx))loadFn(
-#define SSL_FN(lib, ret, name, params, names)	\
+#define SSL_FN(lib, ret, name, params, names)			\
 		ptr_ ## name = (ret (*) params)loadFn(lib, #name);
+#define SSL_FN_ALT(lib, ret, name, params, names, alt)			\
+		ptr_ ## name = (ret (*) params)loadFn(lib, #name, alt);
 #include "OpenSSLFunctions.inc"
 #undef SSL_FN
+#undef SSL_FN_ALT
 	}
 }
 
