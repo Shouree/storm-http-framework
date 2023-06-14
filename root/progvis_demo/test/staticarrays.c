20f91485c6c4a4906670a5edc5393ac432e88837
Filip Strömbäck <fstromback@gmail.com>
2023-06-14 14:33:53 +0200

    Initialization of global arrays work as expected in C in Progvis.

--- root/progvis_demo/test/staticarrays.c
+++ root/progvis_demo/test/staticarrays.c
@@ -1,5 +1,6 @@
 int intarray[4];
 int intarray2[2] = { 1, 2 };
+int intarray3[] = { 1, 2, 3, 4, 5 };
 struct lock lockarray[4];
 int globalinit = 3;
 
@@ -8,5 +9,8 @@ int main() {
 	int localdynarray[z]; // = { 1, 2 };
 	int localstaticarray[1 + 1] = { 1, 2 };
 	int localarray[] = { 1, 2, 3 };
+
+	struct lock *lck = lockarray + 1;
+	lock_init(&lockarray[1]);
 	return 0;
 }
