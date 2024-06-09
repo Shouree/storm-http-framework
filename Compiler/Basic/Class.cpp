Shouree/storm-http-framework

commit 12fc3b636e5ebfb3b6d6ebaf26fd4440dd3cb1f1

Relaxed limitations on actors.
It is now possible to define an actor on any thread, and then let derived
classes restrict it to a particular thread later on in the hierarchy. For
example:

class A on ? {}

class B : extends A, on Compiler {}

This can be used to make the syntax language more thread-agnostic in the future.

Compiler/Basic/Class.cpp
@@ -24,7 +24,7 @@ namespace storm {
 
 		Class::Class(TypeFlags flags, SrcPos pos, Scope scope, Str *name, syntax::Node *body)
 			: Type(name, flags), scope(scope, this), body(body),
-			  otherName(null), otherMeaning(otherNone), allowLazyLoad(true) {
+			  superName(null), threadName(null), threadMeaning(threadNone), allowLazyLoad(true) {
 
 			this->pos = pos;
 		}
@@ -33,37 +33,43 @@ namespace storm {
 		void Class::lookupTypes() {
 			allowLazyLoad = false;
 			try {
-				switch (otherMeaning) {
-				case otherDefaultThread:
-				case otherThread:
-				{
-					// Already done?
-					if (!otherName)
-						break;
-
-					NamedThread *t = as<NamedThread>(scope.find(otherName));
+				// Set the super class first, this makes 'setThread' return true or false as appropriate:
+				if (superName) {
+					Type *t = as<Type>(scope.find(superName));
 					if (!t) {
-						Str *msg = TO_S(engine, S("Can not find the named thread ") << otherName << S("."));
-						throw new (this) SyntaxError(otherName->pos, msg);
+						Str *msg = TO_S(engine, S("Can not find the super class ") << superName << S("."));
+						throw new (this) SyntaxError(superName->pos, msg);
 					}
-
-					setThread(t);
-					break;
-				}
-				case otherSuper:
-				{
-					Type *t = as<Type>(scope.find(otherName));
-					if (!t) {
-						Str *msg = TO_S(engine, S("Can not find the super class ") << otherName << S("."));
-						throw new (this) SyntaxError(otherName->pos, msg);
-					}
-
 					setSuper(t);
-					break;
-				}
 				}
 
-				otherName = null;
+				if (threadMeaning != threadNone) {
+					if (threadName == null) {
+						if (superName) {
+							throw new (this) SyntaxError(
+								superName->pos,
+								S("Can not use 'on ?' together with the 'extends' keyword."));
+						}
+
+						setSuper(TObject::stormType(engine));
+					} else {
+						NamedThread *t = as<NamedThread>(scope.find(threadName));
+						if (!t) {
+							Str *msg = TO_S(engine, S("Can not find the named thread ") << threadName << S("."));
+							throw new (this) SyntaxError(threadName->pos, msg);
+						}
+
+						if (!setThread(t) && threadMeaning != threadDefault) {
+							Str *msg = TO_S(engine, S("Failed to apply the 'on' keyword for this class since ")
+											S("it inherits from a class type, or from an actor that is ")
+											S("already bound to a thread."));
+							throw new (this) SyntaxError(threadName->pos, msg);
+						}
+					}
+				}
+
+				threadName = null;
+				superName = null;
 			} catch (...) {
 				allowLazyLoad = true;
 				throw;
@@ -72,47 +78,36 @@ namespace storm {
 		}
 
 		void Class::super(SrcName *super) {
-			if (otherMeaning == otherThread)
-				throw new (this) SyntaxError(super->pos, S("The 'extends' keyword may not be used together with 'on'."));
-
-			if (otherMeaning == otherSuper)
+			if (superName)
 				throw new (this) SyntaxError(super->pos, S("Only one instance of 'extends' may be used for a single type. ")
 											S("Multiple inheritance is not supported."));
 
-			otherName = super;
-			otherMeaning = otherSuper;
+			superName = super;
 		}
 
 		void Class::thread(SrcName *thread) {
-			if (otherMeaning == otherThread)
+			if (threadMeaning != threadNone && threadMeaning != threadDefault)
 				throw new (this) SyntaxError(thread->pos, S("The 'on' keyword may only be used once."));
 
-			if (otherMeaning == otherSuper)
-				throw new (this) SyntaxError(thread->pos, S("The 'on' keyword may not be used together with 'extends'."));
-
-			otherName = thread;
-			otherMeaning = otherThread;
+			threadName = thread;
+			threadMeaning = threadExplicit;
 		}
 
 		void Class::unknownThread(SrcPos pos) {
-			if (otherMeaning == otherThread)
+			if (threadMeaning != threadNone && threadMeaning != threadDefault)
 				throw new (this) SyntaxError(pos, S("The 'on' keyword may only be used once."));
 
-			if (otherMeaning == otherSuper)
-				throw new (this) SyntaxError(pos, S("The 'on' keyword may not be used together with 'extends'."));
-
-			otherName = null;
-			otherMeaning = otherThread;
-			setSuper(StormInfo<TObject>::type(engine));
+			threadName = null;
+			threadMeaning = threadExplicit;
 		}
 
 		void Class::defaultThread(SrcName *thread) {
 			// Just ignore the default if it is already set!
-			if (otherMeaning != otherNone)
+			if (threadMeaning != threadNone)
 				return;
 
-			otherName = thread;
-			otherMeaning = otherDefaultThread;
+			threadName = thread;
+			threadMeaning = threadDefault;
 		}
 
 		void Class::decorate(SrcName *decorator) {

Compiler/Basic/Class.h
@@ -58,24 +58,24 @@ namespace storm {
 			// Decorators used.
 			MAYBE(Array<SrcName *> *) decorators;
 
-			// Name for either the parent class, or the named thread we shall use. Depending on 'nameIsThread'.
-			MAYBE(SrcName *) otherName;
+			// Name of the parent class, if any.
+			MAYBE(SrcName *) superName;
 
-			// What does 'otherName' mean currently?
-			Byte otherMeaning;
+			// Name of the bound thread, if any.
+			MAYBE(SrcName *) threadName;
+
+			// Meaning of 'threadName'.
+			Byte threadMeaning;
 
 			enum {
-				// Nothing yet.
-				otherNone,
+				// Nothing.
+				threadNone,
 
 				// Default thread to use.
-				otherDefaultThread,
+				threadDefault,
 
-				// Thread to use (non-default, should not be set again). 'otherName' may be null.
-				otherThread,
-
-				// Super class to use.
-				otherSuper,
+				// Set to something explcitly. Might be null, meaning that we should inherit from TObject.
+				threadExplicit,
 			};
 
 			// Allowing lazy loads?

Compiler/Basic/Ctor.cpp
@@ -282,17 +282,16 @@ namespace storm {
 
 			RunOn runOn = thisPtr.type->runOn();
 			bool hiddenThread = runOn.state == RunOn::runtime;
-			if (!hiddenThread && parent == TObject::stormType(engine())) {
-				callTObject(s, runOn.thread);
-				return;
-			}
+			bool implicitThread = runOn.state == RunOn::named && parent->runOn().state == RunOn::runtime;
 
 			// Find something to call.
 			BSNamePart *values = new (this) BSNamePart(Type::CTOR, pos, params);
 			values->alter(0, Value(parent));
 
-			if (hiddenThread)
+			if (hiddenThread || implicitThread) {
+				// We need to insert a Thread as a first parameter.
 				values->insert(storm::thisPtr(Thread::stormType(engine())), 1);
+			}
 
 			Function *ctor = as<Function>(parent->find(values, block->scope));
 			if (!ctor) {
@@ -304,9 +303,13 @@ namespace storm {
 			Array<code::Operand> *actuals = new (this) Array<code::Operand>();
 			actuals->reserve(values->params->count());
 
-			if (hiddenThread) {
+			if (hiddenThread || implicitThread) {
 				actuals->push(params->code(0, s, ctor->params->at(0), block->scope));
-				actuals->push(block->threadParam->var.v);
+				if (hiddenThread) {
+					actuals->push(block->threadParam->var.v);
+				} else {
+					actuals->push(runOn.thread->ref());
+				}
 				for (Nat i = 2; i < values->params->count(); i++)
 					actuals->push(params->code(i - 1, s, ctor->params->at(i), block->scope));
 			} else {
@@ -316,27 +319,6 @@ namespace storm {
 
 			CodeResult *t = new (this) CodeResult();
 			ctor->localCall(s, actuals, t, false);
-		}
-
-		void SuperCall::callTObject(CodeGen *s, NamedThread *t) {
-			if (params->expressions->count() != 1)
-				throw new (this) SyntaxError(pos, S("Can not initialize a threaded object with parameters."));
-
-			// Find the constructor of TObject.
-			Type *parent = TObject::stormType(engine());
-			Array<Value> *values = new (this) Array<Value>(2, Value(parent));
-			values->at(1) = Value(Thread::stormType(engine()));
-			Function *ctor = as<Function>(parent->find(Type::CTOR, values, block->scope));
-			if (!ctor)
-				throw new (this) InternalError(S("The constructor of TObject: __ctor(TObject, Thread) was not found!"));
-
-			// Call the constructor.
-			Array<code::Operand> *actuals = new (this) Array<code::Operand>();
-			actuals->push(params->code(0, s, ctor->params->at(0), block->scope));
-			actuals->push(t->ref());
-
-			CodeResult *res = new (this) CodeResult();
-			ctor->localCall(s, actuals, res, false);
 		}
 
 

Compiler/Basic/Ctor.h
@@ -173,9 +173,6 @@ namespace storm {
 
 			// Scope.
 			CtorBody *block;
-
-			// Call the TObject's ctor, assuming we want to run on 't'.
-			void callTObject(CodeGen *s, NamedThread *t);
 		};
 
 		/**

Compiler/Type.cpp
@@ -373,7 +373,26 @@ namespace storm {
 		return s;
 	}
 
-	void Type::setThread(NamedThread *thread) {
+	Bool Type::setThread(NamedThread *thread) {
+		// Can't set threads for value types.
+		if (value())
+			return false;
+
+		if (Type *currentSuper = super()) {
+			// If the current super-type is something other than the base-class Object, or the base
+			// class has a 'useThread' configured, disallow setting the thread here:
+			Type *objType = Object::stormType(engine);
+			if (isA(objType) && currentSuper != objType) {
+				// We inherit indirectly from a class-type. Don't allow introducing TObject.
+				return false;
+			} else if (isA(TObject::stormType(engine))) {
+				// Disallow if the super type has its 'useThread' set:
+				if (currentSuper->useThread != null) {
+					return false;
+				}
+			}
+		}
+
 		useThread = thread;
 
 		Type *def = defaultSuper();
@@ -383,6 +402,8 @@ namespace storm {
 
 		// Propagate the current thread to any child types.
 		notifyThread(thread);
+
+		return true;
 	}
 
 	RunOn Type::runOn() {

Compiler/Type.h
@@ -98,7 +98,7 @@ namespace storm {
 		MAYBE(Type *) STORM_FN declaredSuper() const;
 
 		// Set the thread for this type. This will force the super-type to be TObject.
-		void STORM_FN setThread(NamedThread *t);
+		Bool STORM_FN setThread(NamedThread *t);
 
 		// Get where we want to run.
 		RunOn STORM_FN runOn();

Compiler/TypeCtor.cpp
@@ -58,21 +58,21 @@ namespace storm {
 		*l << prolog();
 
 		Type *super = owner->super();
-		if (super == TObject::stormType(engine())) {
-			// Run the TObject constructor if possible.
+		if (super && super->runOn().thread == null && runOn().thread != null) {
+			// We are in a situation where we have a specified thread but the parent thread does
+			// not. That means we should inject a parameter specifying the thread to the parent
+			// constructor.
 			NamedThread *thread = runOn().thread;
-			if (!thread) {
-				Str *msg = TO_S(this, S("Can not use TypeDefaultCtor with TObjects without specifying a thread. ")
-								S("Set a thread for ") << owner->identifier() << S(" and try again."));
-				throw new (this) InternalError(msg);
-			}
 
 			Array<Value> *params = new (this) Array<Value>();
 			params->push(thisPtr(super));
 			params->push(thisPtr(Thread::stormType(engine())));
 			Function *ctor = as<Function>(super->find(Type::CTOR, params, Scope()));
-			if (!ctor)
-				throw new (this) InternalError(S("Can not find the default constructor for TObject!"));
+			if (!ctor) {
+				Str *msg = TO_S(this, S("UNable to find a default constructor for the actor ")
+								<< super->identifier() << S(". Can not use TypeDefaultCtor in such cases."));
+				throw new (this) InternalError(msg);
+			}
 
 			*l << fnParam(ptr, me);
 			*l << fnParam(ptr, thread->ref());

commit 7eff1945dcd315f053b10bc9d7b1a2cb51aacc16

Fixed a dependency issue introduced in 12fc3b636e5ebfb3b6d6ebaf26fd4440dd3cb1f1
It caused 'loadAll' to be called too early in some cases, meaning that the code
in 'loadAll' made decisions based on incomplete inheritance- and threading
information.

Fixed by both making 'loadAll' call 'lookupTypes', and by making all type
lookups happen in a scope relative to the parent of the type, rather than the
type itself. Having them relative to the type itself makes the second
lookup (i.e. looking for a thread) look inside the super-class if specified,
which causes it to load too early.

This change likely makes the 'allowLazyLoad' variable unnecessary. It is
retained in case the user does something unexpected (similarly to the code that
ensures 'lookupTypes' is called from 'loadAll').

Compiler/Basic/Class.cpp
@@ -32,6 +32,17 @@ namespace storm {
 
 		void Class::lookupTypes() {
 			allowLazyLoad = false;
+
+			// Lookup super-types and threads relative to the *parent* of the current scope,
+			// otherwise we resolve them relative to the body of the class we are defining, which
+			// does not make sense. It also unneccessary triggers the 'loadAll' in the parent in
+			// case we have both a super-class and a thread set (e.g. when one does 'on Compiler:'
+			// globally in the file). This could make loading happen in an incorrect order, and
+			// cause issues that are difficult to track down.
+			Scope scope = this->scope;
+			if (scope.top)
+				scope.top = scope.top->parent();
+
 			try {
 				// Set the super class first, this makes 'setThread' return true or false as appropriate:
 				if (superName) {
@@ -69,6 +80,7 @@ namespace storm {
 				}
 
 				threadName = null;
+				threadMeaning = threadNone;
 				superName = null;
 			} catch (...) {
 				allowLazyLoad = true;
@@ -126,6 +138,11 @@ namespace storm {
 		bool Class::loadAll() {
 			if (!allowLazyLoad)
 				return false;
+
+			// Make sure that 'lookupTypes' was called. This might not be true in case some type
+			// refers to this type inside its super type or something like that. It is fine to do
+			// this, since 'lookupTypes' is a no-op if it was called already.
+			lookupTypes();
 
 			ClassBody *body = syntax::transformNode<ClassBody, Class *>(this->body, this);
 			body->prepareItems();

Compiler/Basic/Class.h
@@ -25,7 +25,7 @@ namespace storm {
 			// The scope used for this class.
 			Scope scope;
 
-			// Lookup any additional types needed.
+			// Lookup any additional types needed. Note: Must be reentrant!
 			void lookupTypes();
 
 			// Set the super class from a decorator.